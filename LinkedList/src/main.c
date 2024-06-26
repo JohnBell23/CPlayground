dfd31b13e9b4671ad4814b9b155506796d89be59
John Bell <jan@altenter.org>
2024-06-26 21:58:47 +0200

    add linked list u.c.

---
+++ LinkedList/src/main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int main(void) {
+  printf("Hello\n");
+
+  Value v;
+  v.value = 1;
+  Node *startNode = MyLinkedList_init(v);
+
+  for (int i = 2; i < 10; i++) {
+    v.value = i;
+    Node *newNode = MyLinkedList_add(startNode, v);
+  }
+
+  MyLinkedList_printValues(startNode);
+
+  MyLinkedList_destroy(startNode);
+
+  printf("Goodbye\n");
+
+  return 0;
+}
