dd930010c81006d65346fe20b1a9af5b25037991
John Bell <jan@altenter.org>
2024-07-12 14:40:42 +0200

    ADD empty default eclipse project

---
+++ DefaultProject/inc/main.h
@@ -0,0 +1,13 @@
+/*
+ * main.c
+ *
+ *  Created on: 12.07.2024
+ *      Author: jlue4
+ */
+
+#ifndef MAIN_C_
+#define MAIN_C_
+
+
+
+#endif /* MAIN_C_ */
---
+++ DefaultProject/src/main.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : DefaultProject.c
+ Author      : John Bell
+ Version     :
+ Copyright   : @2024
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
